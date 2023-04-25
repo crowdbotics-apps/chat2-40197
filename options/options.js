50bd8c5a91a374c51b6400507139d09923202b65
Guilherme Livramento <guilherme.livramento.fsl@crowdbotics.com>
2023-04-25 09:37:37 -0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "chat2-40197",
-  url: "https://chat2-40197.botics.co",
-  api: "https://chat2-40197.botics.co/api/v1"
-}
+  "name": "chat2-40197",
+  "url": "https://chat2-40197.botics.co",
+  "api": "https://chat2-40197.botics.co/api/v1",
+  "initialRoute": "Chat"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
